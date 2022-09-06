f5d5b0970d8a5be01cb93b39fc84180e1e006891
Ayush Suman <ayush231suman@gmail.com>
2022-09-07 01:37:17 +0530

    New website home screen

---
+++ assets/scripts/index.js
@@ -0,0 +1,52 @@
+const TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    const that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    const element = document.getElementById('typewrite');
+        const toRotate = element.getAttribute('type-messages');
+        const period = 2000;
+        if (toRotate) {
+            new TxtType(element, JSON.parse(toRotate), period);
+        }
+    const css = document.createElement("style");
+    css.innerHTML = "#typewrite > .wrap { display: inline; margin-left: 0; }";
+    document.body.appendChild(css);
+};
